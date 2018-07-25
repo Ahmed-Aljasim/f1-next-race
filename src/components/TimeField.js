9b232c64ef0f6fa568b1ee7e692e85a1b03aa55e
AJ <ahmed1700@windowslive.com>
2018-07-25 17:12:12 +0300

    changed app name and fixed bugs related to months are zero based

--- src/components/TimeField.js
+++ src/components/TimeField.js
@@ -27,19 +27,27 @@ class TimeField extends Component {
     // Current date
     const currentDate = now.toISOString().split('T')[0];
     const c = currentDate.split('-');
+    // Months are zero based
+    c[1] = c[1] - 1;
 
     // Race date
     const raceDate = this.props.date;
     const r = raceDate.split('-');
+    // Months are zero based
+    r[1] = r[1] - 1;
 
     // Create moment object and find the diff
     const x = new moment({ y: r[0], M: r[1], d: r[2], h: t[0], m: t[1], s: 0, ms: 0 });
     const y = new moment({ y: c[0], M: c[1], d: c[2], h: cH, m: cM, s: cS, ms: 0 });
     const duration = moment.duration(x.diff(y));
+    
+    let monthsToDays = 0;
+    // duration._data.months should be converted to days
+    if (duration._data.months !== 0 ) monthsToDays = Math.abs(moment().diff(raceDate, 'days'));
 
     // Set the states to the new values
     this.setState({
-      d: duration._data.days,
+      d: duration._data.days + monthsToDays, 
       h: duration._data.hours,
       m: duration._data.minutes,
       s: duration._data.seconds
